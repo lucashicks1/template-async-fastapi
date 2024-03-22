b571414884e242111b7e995de4df6e225da6f011
lucashicks1 <lucasjhicks1@gmail.com>
2024-03-23 00:10:38 +1000

    fix: Adds ArtistModel to __init__.py file

--- fastapi_clean/models/__init__.py
+++ fastapi_clean/models/__init__.py
@@ -1,4 +1,5 @@
 from .base_model import BaseModel
 from .song_model import SongModel
+from .artist_model import ArtistModel
 
-__all__ = ["BaseModel", "SongModel"]
+__all__ = ["BaseModel", "SongModel", "ArtistModel"]
