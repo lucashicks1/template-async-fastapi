97bce8cbef9236288f5f7f8ea3fd012a5c6cc5fc
lucashicks1 <lucasjhicks1@gmail.com>
2024-03-23 10:41:16 +1000

    new: Adds more functionality to base repository

--- template_async_fastapi/repositories/base_repository.py
+++ template_async_fastapi/repositories/base_repository.py
@@ -5,6 +5,7 @@ from template_async_fastapi.models.base_model import BaseModel
 from template_async_fastapi.database import DB
 from fastapi import Depends
 from pydantic import BaseModel as SchemaBaseModel
+from template_async_fastapi.exceptions import NotFoundException
 
 Model = TypeVar("Model", bound=BaseModel)
 
@@ -18,10 +19,10 @@ class BaseRepository(ABC, Generic[Model]):
         self.model = model
 
     @abstractmethod
-    async def get(self, uuid: UUID) -> SchemaBaseModel | None:
+    async def get(self, uuid: UUID) -> SchemaBaseModel:
         result: Model | None = await self.db.get(self.model, uuid)
         if not result:
-            return None
+            raise NotFoundException()
         return SchemaBaseModel(**result.normalise())
 
     @abstractmethod
@@ -33,12 +34,24 @@ class BaseRepository(ABC, Generic[Model]):
         return SchemaBaseModel(**model_instance.normalise())
 
     @abstractmethod
-    async def update(self, uuid: UUID, data: dict) -> SchemaBaseModel | None:
+    async def update(self, uuid: UUID, data: dict) -> SchemaBaseModel:
         result: Model | None = await self.db.get(self.model, uuid)
         if not result:
-            return None
+            raise NotFoundException
         for key, value in data.items():
             setattr(result, key, value)
         await self.db.commit()
         await self.db.refresh(result)
         return SchemaBaseModel(**result.normalise())
+
+    @abstractmethod
+    async def delete(self, uuid: UUID) -> None:
+        result: Model | None = await self.db.get(self.model, uuid)
+        if not result:
+            raise NotFoundException
+        await self.db.delete(result)
+        await self.db.commit()
+
+
+
+
